52c1a8de7bffb6bee3c338e0056e837cac461c65
Felipe Czerniak <116464268+FelipeCzerniak5070@users.noreply.github.com>
2025-04-17 23:42:28 +0000

    Segundo Commit

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,30 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
-import * as fs from 'fs';
-import * as path from 'path';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
 
 @Component({
   selector: 'app-main',
-  imports: [ButtonModule,InputTextModule],
+  imports: [ButtonModule,InputTextModule, BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule, Menubar ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
-
-
-  criarArquivoVazio(){
-    const nomeArquivo = 'teste.txt';
-    const caminhoArquivo = path.join(__dirname, nomeArquivo); // Define o caminho do arquivo no diretório atual
-  
-    fs.writeFile(caminhoArquivo, '', (err) => {
-      if (err) {
-        console.error('Erro ao criar o arquivo:', err);
-      } else {
-        console.log(`Arquivo "${nomeArquivo}" criado com sucesso em: ${caminhoArquivo}`);
-      }
-    });
-  };
+  items: MenuItem[] | undefined;
 
 }
